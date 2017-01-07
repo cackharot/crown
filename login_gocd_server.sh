c076cafaf06f14c635dd345404bd3b89c9bd9f62
cackharot <cackharot@gmail.com>
2017-01-07 12:52:27 -0500

    updated addons setup and use presistent volumes for gocd-server storage

---
+++ login_gocd_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kubectl exec -ti gocd-server-1926623619-z5bv0 --namespace=chaos-gocd -- bash
--- setup_addons.sh
+++ setup_addons.sh
@@ -16,8 +16,12 @@ echo 'Setup cluster addons'
 install() {
     kubectl create -f "${KUBE_ROOT}/$1.yml" --record
     service_file="${KUBE_ROOT}/$1-service.yml"
+    volume_file="${KUBE_ROOT}/$1-volume.yml"
     if [ -f "${service_file}"  ]; then
-        kubectl create -f "${KUBE_ROOT}/$1-service.yml" --record
+        kubectl create -f "${service_file}" --record
+    fi
+    if [ -f "${volume_file}"  ]; then
+        kubectl create -f "${volume_file}" --record
     fi
 }
 
@@ -31,7 +35,7 @@ create_namespace() {
 }
 
 install_gocd_ns_certs () {
-    echo 'Creating kube secret for docker registry certifications'
+    echo 'Creating kube secret for docker registry certifications in gocd-agents'
     ${KUBECTL} --namespace=chaos-gocd delete secret registry-tls-secret
     ${KUBECTL} --namespace=chaos-gocd create secret generic registry-tls-secret \
         --from-file=ca.crt=certs/registry.walkure.net.crt
@@ -45,9 +49,6 @@ install_certs () {
         --from-file=domain.crt=certs/registry.walkure.net.crt \
         --from-file=domain.key=certs/registry.walkure.net.key
 }
-
-create_namespace 'chaos-gocd'
-install_gocd_ns_certs
 
 if [ $ENABLE_DOCKER_REGISTRY = true ]; then
     KUBEREGISTRY=`eval "${KUBECTL} get services --namespace=kube-system | grep kube-registry | cat"`
@@ -76,6 +77,8 @@ if [ $ENABLE_GOCD = true ]; then
     GOCD_SERVER=`eval "${KUBECTL} get services --namespace=chaos-gocd | grep gocd-server | cat"`
     if [ ! "${GOCD_SERVER}" ]; then
         echo 'Creating GoCD server and agents'
+        create_namespace 'chaos-gocd'
+        install_gocd_ns_certs
         install 'gocd-server'
         install 'gocd-agent'
     else
@@ -84,17 +87,34 @@ if [ $ENABLE_GOCD = true ]; then
 fi
 
 if [ $ENABLE_REGISTRY_UI = true ]; then
-    echo 'Creating docker registry ui'
-    install 'registry-ui'
+    HAS_REGISTRY_UI=`eval "${KUBECTL} get services --namespace=kube-system| grep registry-ui | cat"`
+     if [ ! "${HAS_REGISTRY_UI}" ]; then
+        echo 'Creating docker registry ui'
+        install 'registry-ui'
+    else
+        echo 'Docker registry UI already installed. Skipping'
+    fi
 fi
 
 if [ $ENABLE_DASHBOARD = true ]; then
-    echo 'Creating kubernetes ui dashboard'
-    install 'dashboard'
+    HAS_KUBE_DASHBOARD=`eval "${KUBECTL} get services --namespace=kube-system| grep dashboard | cat"`
+     if [ ! "${HAS_KUBE_DASHBOARD}" ]; then
+        echo 'Creating kubernetes dashboard'
+        install 'dashboard'
+    else
+        echo 'Kubernetes dashboard already installed. Skipping'
+    fi
 fi
 
 if [ $ENABLE_ELK = true ]; then
-    echo 'Creating ELK stack'
-    #install 'logstash'
-    #install 'kibana'
+    GOCD_SERVER=`eval "${KUBECTL} get services --namespace=chaos-elk | grep elasticsearch | cat"`
+    if [ ! "${GOCD_SERVER}" ]; then
+        echo 'Creating ELK stack'
+        create_namespace 'chaos-elk'
+        #install 'elasticsearch'
+        #install 'logstash'
+        #install 'kibana'
+    else
+        echo 'ELK already installed. Skipping'
+    fi
 fi
